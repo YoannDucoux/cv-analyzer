a57a981a9390229d4f287c6eea4ea4e87e703009
Yoann Ducoux <137491158+YoannDucoux@users.noreply.github.com>
2025-12-18 15:55:14 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,17 +6,18 @@ app = FastAPI(title="CV Analyzer", version="0.1.0")
 
 # MVP: CORS ouvert pour permettre au front local d'appeler l'API.
 # En production, on restreindra allow_origins à ton domaine.
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-  "https://ton-frontend.netlify.app",
-  "https://ton-frontend.vercel.app"
-],
-,
+        "https://6942d3e2bde5e3860c376944--cv-analyzer-api.netlify.app",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST", "GET"],
     allow_headers=["*"],
 )
+
 
 app.include_router(router, prefix="/api/v1")
 
